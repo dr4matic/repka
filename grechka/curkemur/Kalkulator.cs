4766eba1f40a1a07e166f7f8e2d6df2dff218bbb
dr4matic <loginovzaxar04@gmail.com>
2022-09-24 14:08:19 +0300

    lala

--- grechka/curkemur/Kalkulator.cs
+++ grechka/curkemur/Kalkulator.cs
@@ -12,30 +12,53 @@ namespace curkemur
         {
             decimal chisla = 0;
             var numbers = ParsString(expresion);
-            for (var i=0;i<numbers.Length;i++)
+            /*for (var i=0;i<numbers.Count;i++)
             {
                  chisla = chisla + numbers[i];
+            }*/
+            foreach(var seledka in numbers)
+            {
+                chisla = chisla + seledka;
             }
             return chisla;
         }
-        private decimal[] ParsString(string expresion)
+        private List<decimal> ParsString(string expresion)
         {   var kretin = expresion.Split('+');
             var result = new List<decimal>(kretin.Length);
             for (var i=0;i<kretin.Length;i++)
             {
-                if (result.Contains('-'))
+                if (kretin[i].Contains('-'))
                 {
-
+                    var pepe = ParsStringMinus(kretin[i]);
+                    result.AddRange(pepe);
                 }
                 else 
                 {
-
-                }
                 var kek = decimal.Parse(kretin[i]);
                 result.Add(kek);
+                }
+                
             }
-            return result.ToArray();
+            return result;
         }
-       
+        private List<decimal> ParsStringMinus(string expresion)
+        {
+            var kretin = expresion.Split('-');
+            var result = new List<decimal>(kretin.Length);
+            var min = 1;
+            for (var i = 0; i < kretin.Length; i++)
+            {
+                if (kretin[i].Length == 0)
+                {
+                    min = -1;
+                    continue;
+                }
+                var kek = decimal.Parse(kretin[i])*min;
+                result.Add(kek);
+                min = -1;
+            }
+            return result;
+        }
+
     }
 }
