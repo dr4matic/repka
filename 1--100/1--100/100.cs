f4d6e8ddc8adb8c3557749b7bfe1fb2f8a0982f0
dr4matic <loginovzaxar04@gmail.com>
2022-10-22 11:02:57 +0300

    reli

---
+++ 1--100/1--100/100.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1__100
+{
+    class _100
+    {
+        public void Print<B>(B znach)
+        {
+            var glek = Console.BackgroundColor;
+            if (typeof(string) == znach.GetType())
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+            else
+            if (typeof(int) == znach.GetType())
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+            }
+            Console.WriteLine(znach);
+            Console.BackgroundColor = glek;
+        }
+
+    }
+}
---
+++ 1--100/1--100/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1__100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            _100 pepe = new _100();
+            for (var i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    pepe.Print((char)i);
+                }
+                else if (i % 2 == 0)
+                {
+                    pepe.Print(i.ToString());
+                }
+                else
+                {
+                    pepe.Print(i);
+                }
+            }
+        }
+    }
+}
